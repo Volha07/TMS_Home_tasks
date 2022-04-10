676a28f5c5eed35d895e14b07bb353905e0031bb
Volha07 <102689327+Volha07@users.noreply.github.com>
2022-04-11 01:29:27 +0300

    Add file Scanner

---
+++ Program_User.java
@@ -0,0 +1,11 @@
+public class Program_User {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello <user>");
+
+        String name = scanner.nextLine(User name);
+    }
+
+}
